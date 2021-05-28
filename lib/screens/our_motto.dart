ea0b65ff0b46a83d55054dc9b34372968c3792b0
Procoder16 <smsoumik1608@gmail.com>
2021-05-28 18:47:18 +0530

    done with the appBar of the our motto page

--- lib/screens/our_motto.dart
+++ lib/screens/our_motto.dart
@@ -3,6 +3,28 @@ import 'package:flutter/material.dart';
 class OurMotto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.first_page,
+            color: Color(0xFF11212F),
+          ),
+          iconSize: 30.0,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Color(0xFF84EFD5),
+        centerTitle: true,
+        title: Text(
+          "Our Motto",
+          style: TextStyle(
+            color: Color(0xFF11212F),
+            fontSize: 22.0,
+          ),
+        ),
+      ),
+    );
   }
 }
