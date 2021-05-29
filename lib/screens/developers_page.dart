1ce74f0d852ba3b909ca43aed4d124b4e755a7fe
Procoder16 <smsoumik1608@gmail.com>
2021-05-29 17:02:43 +0530

    done with the developers_page.dart

--- lib/screens/developers_page.dart
+++ lib/screens/developers_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DevelopersPage extends StatelessWidget {
   @override
@@ -39,9 +42,97 @@ class DevelopersPage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Container(
-          child: Column(
-            children: [],
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 180.0,
+                    width: 180.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('images/myPic.png'),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(
+                        'Soumik Mukherjee',
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        'Soumik is a B.Tech (CSE) student at KiiT, Bhubaneshwar. He has keen interest in the field of development. He also has good problem solving abilities. He is a student with a positive attitude towards everything. Feel free to connect to him through the given links below.',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 25.0,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Soumik's LinkedIn Profile",
+                              style:
+                                  TextStyle(fontSize: 20.0, color: Colors.blue),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () async {
+                                  var url =
+                                      "https://www.linkedin.com/in/soumik-mukherjee-438b451b5/";
+                                  if (await canLaunch(url)) {
+                                    await launch(url);
+                                  } else {
+                                    throw "Failed to open LinkedIn";
+                                  }
+                                },
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Soumik's GitHub Profile",
+                              style:
+                                  TextStyle(fontSize: 20.0, color: Colors.blue),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () async {
+                                  var url = "https://github.com/Procoder16";
+                                  if (await canLaunch(url)) {
+                                    await launch(url);
+                                  } else {
+                                    throw "Failed to open GitHub";
+                                  }
+                                },
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
