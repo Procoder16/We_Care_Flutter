ed8732289bd0f3dcb7ea33608dc6e7b6bdebe4a4
Procoder16 <smsoumik1608@gmail.com>
2021-05-31 18:15:45 +0530

    done with the our team page

--- lib/screens/about_us.dart
+++ lib/screens/about_us.dart
@@ -64,7 +64,7 @@ class OurMotto extends StatelessWidget {
                         children: [
                           Text(
                             'Our is motto is nothing different than the name of our organisation. We Care... as the name suggests, we really do care about mankind and specially for them who cannot care for themselves due to some reason or the other.',
-                            style: TextStyle(fontSize: 18.0),
+                            style: TextStyle(fontSize: 20.0),
                           ),
                           SizedBox(
                             height: 10.0,
@@ -72,7 +72,7 @@ class OurMotto extends StatelessWidget {
                           Text(
                             'We started this organisation casually one day. Me and some of my friends, while having a conversation about shopping one day, diverted to some other thing and that was, our old clothes. We tend to buy new clothes from time to time thereby filling and stuffing our cupboards with clothes. After some time we realise that we don\'t need them but still we keep them as it is. So we decided to donate those old clothes to them who really need it. We gathered some old clothes from our locality also and went to a nearby railway station and gave them to the people who don\'t even get even food for a living. Their smile after getting those clothes was unmatchable and therefore we made our minds and decided to continue this work.',
                             style: TextStyle(
-                              fontSize: 18.0,
+                              fontSize: 20.0,
                             ),
                           ),
                           SizedBox(
@@ -81,7 +81,7 @@ class OurMotto extends StatelessWidget {
                           Text(
                             'From then on, we donate clothes, food, essentials like sanitary pad, etc. to these people and also to poor people who approach us for help and to those who can\'t afford these necessary things. We even organise blood donation camp so that people can come forward to donate blood that can literally save someone\'s life.',
                             style: TextStyle(
-                              fontSize: 18.0,
+                              fontSize: 20.0,
                             ),
                           ),
                           SizedBox(
@@ -90,7 +90,7 @@ class OurMotto extends StatelessWidget {
                           Text(
                             'When we started as a team, we were not that large and famous as a team. We were just 7 of us. Now we are a much larger team of about 100 individuals and even, we have gained some popularity and recognition as an organisation and we need this recognition, not to establish ourselves as a celebrity, but for the establishment of this organisation so that more and more people get to know about us and help us in this noble cause.',
                             style: TextStyle(
-                              fontSize: 18.0,
+                              fontSize: 20.0,
                             ),
                           ),
                         ],
