8b2d43fe37534afe632189977849b3cc0e496098
Procoder16 <smsoumik1608@gmail.com>
2021-05-31 18:05:09 +0530

    done with the our team page

--- lib/screens/our_team.dart
+++ lib/screens/our_team.dart
@@ -41,12 +41,42 @@ class OurTeam extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(200.0),
-              ),
-              child: Image(
-                image: AssetImage('images/group.png'),
+            Expanded(
+              child: ListView(
+                children: [
+                  Column(
+                    children: [
+                      Image(
+                        image: AssetImage('images/group.png'),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'This is our core team. We people manage everything about our organisation. We have divided the work among us equally so that no one sits idle or no one is burdened with all the work. We have divided ourselves under various committees like the communications, management, fund collection, social media marketing, public announcements, cooking, marketing, etc.',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              'The management committee is headed by Soumik, communications is headed by Ritam, fund collection is headed by Swapnil, social media marketing is headed by Aniket, public announcements is headed by Shubham, cooking is headed by Sushreet, marketing is headed by Rahul and fund collection is headed by Rittik.',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Text(
+                              'Anyone interested in joining or team may directly contact Soumik by visiting the Developer\'s Page.',
+                              style: TextStyle(fontSize: 20.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ],
