bd5130995d2f9bdec37bd166ffc29e38c1c693db
Procoder16 <smsoumik1608@gmail.com>
2021-05-31 16:39:35 +0530

    done with the our motto page

--- lib/screens/our_motto.dart
+++ lib/screens/our_motto.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
@@ -49,6 +50,31 @@ class OurMotto extends StatelessWidget {
                   fontFamily: 'Kaushan',
                   fontSize: 45.0,
                   color: Color(0xFF84EFD5),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Our is motto is nothing different than the name of our organisation. We Care... as the name suggests, we really do care about mankind and specially for them who cannot care for themselves due to some reason or the other.',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'We started this organisation casually one day. Me and some of my friends, while having a conversation about shopping one day, diverted to some other thing and that was, our old clothes. We tend to buy new clothes from time to time thereby filling and stuffing our cupboards with clothes. After some time we realise that we don\'t need them but still we keep them as it is. So we decided to donate those old clothes to them who really need it. We gathered some old clothes from our locality also and went to a nearby railway station and gave them to the people who don\'t even get even food for a living. Their smile after getting those clothes was unmatchable and therefore we made our minds and decided to continue this work.',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
