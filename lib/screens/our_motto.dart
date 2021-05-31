cdda354c3ba4423e26c573dd140c4ba20a11b3a4
Procoder16 <smsoumik1608@gmail.com>
2021-05-31 16:45:50 +0530

    made the text in a list view and scrollable

--- lib/screens/our_motto.dart
+++ lib/screens/our_motto.dart
@@ -53,25 +53,38 @@ class OurMotto extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height: 10.0,
+                height: 15.0,
               ),
               Expanded(
                 child: Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: Column(
+                  child: ListView(
                     children: [
-                      Text(
-                        'Our is motto is nothing different than the name of our organisation. We Care... as the name suggests, we really do care about mankind and specially for them who cannot care for themselves due to some reason or the other.',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      Text(
-                        'We started this organisation casually one day. Me and some of my friends, while having a conversation about shopping one day, diverted to some other thing and that was, our old clothes. We tend to buy new clothes from time to time thereby filling and stuffing our cupboards with clothes. After some time we realise that we don\'t need them but still we keep them as it is. So we decided to donate those old clothes to them who really need it. We gathered some old clothes from our locality also and went to a nearby railway station and gave them to the people who don\'t even get even food for a living. Their smile after getting those clothes was unmatchable and therefore we made our minds and decided to continue this work.',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
+                      Column(
+                        children: [
+                          Text(
+                            'Our is motto is nothing different than the name of our organisation. We Care... as the name suggests, we really do care about mankind and specially for them who cannot care for themselves due to some reason or the other.',
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'We started this organisation casually one day. Me and some of my friends, while having a conversation about shopping one day, diverted to some other thing and that was, our old clothes. We tend to buy new clothes from time to time thereby filling and stuffing our cupboards with clothes. After some time we realise that we don\'t need them but still we keep them as it is. So we decided to donate those old clothes to them who really need it. We gathered some old clothes from our locality also and went to a nearby railway station and gave them to the people who don\'t even get even food for a living. Their smile after getting those clothes was unmatchable and therefore we made our minds and decided to continue this work.',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            'From then on, we donate clothes, food, essentials like sanitary pad, etc. to these people and also to poor people who approach us for help and to those who can\'t afford these necessary things. We even organise blood donation camp so that people can come forward to donate blood that can literally save someone\'s life.',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
