eea0272c4b53a100a6efac04c57aa5636cc1820a
Procoder16 <smsoumik1608@gmail.com>
2021-06-01 20:16:12 +0530

    added the phone and mail option also

--- lib/screens/contact_us.dart
+++ lib/screens/contact_us.dart
@@ -40,95 +40,117 @@ class ContactUs extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 10.0,
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Want to contact for further information?',
+              style: TextStyle(
+                fontSize: 20.0,
               ),
-              Text(
-                'Want to contact for further information?',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Now, since the time we have started for for this, we have been getting a lot of DMs on various platforms regarding the procedure for joining the organising, what and how to donate and when to donate. People also ask some other doubts that they want to clear. For all of them, here is the list of platforms, where you can contact us directly.',
-                      style: TextStyle(
-                        fontSize: 18.0,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Now, since the time we have started for for this, we have been getting a lot of DMs on various platforms regarding the procedure for joining the organising, what and how to donate and when to donate. People also ask some other doubts that they want to clear. For all of them, here is the list of platforms, where you can contact us directly.',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Column(
+                    children: [
+                      GestureDetector(
+                        child: Icon(
+                          FontAwesomeIcons.instagram,
+                          size: 60.0,
+                        ),
+                        onTap: () async {
+                          const url =
+                              'https://www.instagram.com/blogs_by_soumik/?hl=en';
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
                       ),
-                    ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Column(
-                      children: [
-                        GestureDetector(
-                          child: Icon(
-                            FontAwesomeIcons.instagram,
-                            size: 60.0,
-                          ),
-                          onTap: () async {
-                            const url =
-                                'https://www.instagram.com/blogs_by_soumik/?hl=en';
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          },
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        child: Icon(
+                          FontAwesomeIcons.linkedin,
+                          size: 60.0,
                         ),
-                        SizedBox(
-                          height: 20.0,
+                        onTap: () async {
+                          const url =
+                              'https://www.linkedin.com/in/soumik-mukherjee-438b451b5/';
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        child: Icon(
+                          FontAwesomeIcons.twitter,
+                          size: 60.0,
                         ),
-                        GestureDetector(
-                          child: Icon(
-                            FontAwesomeIcons.linkedin,
-                            size: 60.0,
-                          ),
-                          onTap: () async {
-                            const url =
-                                'https://www.linkedin.com/in/soumik-mukherjee-438b451b5/';
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          },
+                        onTap: () async {
+                          const url =
+                              'https://twitter.com/SoumikM40838957?s=09';
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            throw 'Could not launch $url';
+                          }
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        child: Icon(
+                          FontAwesomeIcons.phoneVolume,
+                          size: 60.0,
                         ),
-                        SizedBox(
-                          height: 20.0,
+                        onTap: () {
+                          launch("tel://+91 9876543210");
+                        },
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      GestureDetector(
+                        child: Icon(
+                          Icons.mail_outlined,
+                          size: 60.0,
                         ),
-                        GestureDetector(
-                          child: Icon(
-                            FontAwesomeIcons.twitter,
-                            size: 60.0,
-                          ),
-                          onTap: () async {
-                            const url =
-                                'https://twitter.com/SoumikM40838957?s=09';
-                            if (await canLaunch(url)) {
-                              await launch(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                        onTap: () {
+                          launch("mailto:abc123@gmail.com");
+                        },
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
