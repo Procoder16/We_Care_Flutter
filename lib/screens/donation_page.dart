7c56250ca06f5b70916201fd42515e0cfa106693
Procoder16 <smsoumik1608@gmail.com>
2021-06-02 16:10:33 +0530

    made the UI of the donation page

--- lib/screens/donation_page.dart
+++ lib/screens/donation_page.dart
@@ -38,6 +38,82 @@ class DonationPage extends StatelessWidget {
           ),
         ],
       ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                'We accept donation in any form. People contact us for donating money, grains, pulses, rice, clothes, spices, etc. Anyone interested to make a contribution from their end please come forward. It would be highly appreciable.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'If you want to make your contribution through money, click the button below.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: RaisedButton(
+                  color: Color(0xFF84EFD5),
+                  child: Text(
+                    'Monetary Donation',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF11212F),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'If you want to make your contribution by directly donating grains, pulses, clothes, etc., you can directly contact us and our team will reach your home to pick them up. Click the button below to head over to the Contacts Page.',
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: RaisedButton(
+                  color: Color(0xFF84EFD5),
+                  child: Text(
+                    'Other Donations',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF11212F),
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                'Thanks a lot for your valuable donation 🙏',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
