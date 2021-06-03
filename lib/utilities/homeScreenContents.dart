ecb79c47ac94b71354d753f07529da1f6b966a04
Procoder16 <smsoumik1608@gmail.com>
2021-06-03 17:11:35 +0530

    images applied to the cards

--- lib/utilities/homeScreenContents.dart
+++ lib/utilities/homeScreenContents.dart
@@ -37,7 +37,7 @@ class HomeScreenContents extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5.0),
                           child: Image(
-                            image: AssetImage('images/group.png'),
+                            image: AssetImage('images/mask.png'),
                           ),
                         ),
                         Padding(
@@ -84,7 +84,7 @@ class HomeScreenContents extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5.0),
                           child: Image(
-                            image: AssetImage('images/group.png'),
+                            image: AssetImage('images/food1.png'),
                           ),
                         ),
                         Padding(
@@ -131,7 +131,7 @@ class HomeScreenContents extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5.0),
                           child: Image(
-                            image: AssetImage('images/group.png'),
+                            image: AssetImage('images/clothes.png'),
                           ),
                         ),
                         Padding(
@@ -178,7 +178,148 @@ class HomeScreenContents extends StatelessWidget {
                         Padding(
                           padding: EdgeInsets.symmetric(horizontal: 5.0),
                           child: Image(
-                            image: AssetImage('images/group.png'),
+                            image: AssetImage('images/sanitary.png'),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '20/05/2021',
+                                style: kCardsText,
+                              ),
+                              Text(
+                                'Barrackpore',
+                                style: kCardsText,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 5.0),
+                          child: Text(
+                            'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj',
+                            style: kCardsText,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0),
+                child: Card(
+                  color: Color(0xFF11292F),
+                  elevation: 10.0,
+                  shadowColor: Color(0xFF84EFD5),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Image(
+                            image: AssetImage('images/last.jpg'),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '20/05/2021',
+                                style: kCardsText,
+                              ),
+                              Text(
+                                'Barrackpore',
+                                style: kCardsText,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 5.0),
+                          child: Text(
+                            'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj',
+                            style: kCardsText,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0),
+                child: Card(
+                  color: Color(0xFF11292F),
+                  elevation: 10.0,
+                  shadowColor: Color(0xFF84EFD5),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Image(
+                            image: AssetImage('images/bloodD.png'),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '20/05/2021',
+                                style: kCardsText,
+                              ),
+                              Text(
+                                'Barrackpore',
+                                style: kCardsText,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 5.0, horizontal: 5.0),
+                          child: Text(
+                            'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj',
+                            style: kCardsText,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0),
+                child: Card(
+                  color: Color(0xFF11292F),
+                  elevation: 10.0,
+                  shadowColor: Color(0xFF84EFD5),
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.0),
+                          child: Image(
+                            image: AssetImage('images/food2.png'),
                           ),
                         ),
                         Padding(
