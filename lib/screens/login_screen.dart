5c161c0e085cc3eed1f075858891e492a1e16fc4
Procoder16 <smsoumik1608@gmail.com>
2021-06-07 18:42:44 +0530

    made the basic login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,8 +6,34 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: Column(
-            children: [Text('User')],
+          child: Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Column(
+              children: [
+                Text(
+                  'User Authentication',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'Kaushan',
+                  ),
+                ),
+                Text(
+                  'Click the button below to verify your Google Account 👇',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                RaisedButton(
+                  child: Text(
+                    'Tap to verify',
+                    style: TextStyle(
+                      color: Color(0xFF11212F),
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  color: Color(0xFF84EFD5),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
